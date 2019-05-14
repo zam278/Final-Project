85f517bbe7a4e706bbdda40964dbc19464b909c7
Zee <zam278@nyu.edu>
2019-05-14 11:00:56 -0400

    calling geojson files

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,27 @@ var map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 
 //Get modal element
-var modal = document.getElementById('simpleModal');
+var modal = document.getElementsByClassName('simpleModal');
 //get close button
-var closeBtn = document.getElementById('closeBtn');
+var closeBtn = document.getElementByClassName('closeBtn'); [0];
 
 //Listen for close click
 closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
 // Function to close the modal
 function closeModal (){
   modal.style.display = 'none';
 }
+// Function to close the modal if outside click
+function outsideClick (e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
+
+// call geojson files
+$.getJSON('subway_lines'.geojson, function(data) {
+	// the data is now available inside this function only as 'data'
+	console.log(data);
+})
