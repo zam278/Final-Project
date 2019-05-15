d5b9b95e74ca1f50489e09441654340f93932e83
Zee <zam278@nyu.edu>
2019-05-15 19:46:02 -0400

    sidebar fixed

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmY
 var map = new mapboxgl.Map({
   container: 'mapContainer',
   style: 'mapbox://styles/mapbox/dark-v9',
-  center: [-73.968544,40.734771],
+  center: [-73.954639,40.697429],
   zoom: 12,
 });
 
